3383c964f4af6acb31a18e890d30bda074a7ba7d
Steven Lee <stevenyslee@gmail.com>
2019-01-05 17:30:15 -0800

    Test to copy description

--- Entity.test.js
+++ Entity.test.js
@@ -224,6 +224,80 @@ describe('Entity', () => {
     expect(testEntity.entity).toMatchObject(expected);
   });
 
+  test('Graph has parent with two children', () => {
+    entityId = 2;
+    entity = {
+      "entities": [{
+        "entity_id": 1,
+        "name": "EntityA"
+      }, {
+        "entity_id": 2,
+        "name": "EntityB"
+      }, {
+        "entity_id": 3,
+        "name": "EntityC",
+      }, {
+        "entity_id": 4,
+        "name": "EntityD",
+      }],
+      "links": [{
+        "from": 1,
+        "to": 2
+      }, {
+        "from": 2,
+        "to": 3
+      }, {
+        "from": 2,
+        "to": 4
+      }]
+    };
+    expected = {
+      "entities": [{
+        "entity_id": 1,
+        "name": "EntityA"
+      }, {
+        "entity_id": 2,
+        "name": "EntityB"
+      }, {
+        "entity_id": 3,
+        "name": "EntityC",
+      }, {
+        "entity_id": 4,
+        "name": "EntityD"
+      }, {
+        "entity_id": 5,
+        "name": "EntityB"
+      }, {
+        "entity_id": 6,
+        "name": "EntityC"
+      }, {
+        "entity_id": 7,
+        "name": "EntityD"
+      }],
+      "links": [{
+        "from": 1,
+        "to": 2
+      }, {
+        "from": 2,
+        "to": 3
+      }, {
+        "from": 2,
+        "to": 4
+      }, {
+        "from": 1,
+        "to": 5
+      }, {
+        "from": 5,
+        "to": 6
+      }, {
+        "from": 5,
+        "to": 7
+      }]
+    };
+    testEntity = new Entity(entity, entityId);
+    expect(testEntity.entity).toMatchObject(expected);
+  });
+
   test('Start cloning at graph head', () => {
     entityId = 1;
     entity = {
@@ -277,6 +351,70 @@ describe('Entity', () => {
       }, {
         "from": 4,
         "to": 6
+      }]
+    };
+    testEntity = new Entity(entity, entityId);
+    expect(testEntity.entity).toMatchObject(expected);
+  });
+
+  test('Test to copy description', () => {
+    entityId = 2;
+    entity = {
+      "entities": [{
+        "entity_id": 1,
+        "name": "EntityA",
+        "description": "More about EntityA"
+      }, {
+        "entity_id": 2,
+        "name": "EntityB",
+        "description": "More about EntityB"
+      }, {
+        "entity_id": 3,
+        "name": "EntityC",
+        "description": "More about EntityC"
+      }],
+      "links": [{
+        "from": 1,
+        "to": 2
+      }, {
+        "from": 2,
+        "to": 3
+      }]
+    };
+    expected = {
+      "entities": [{
+        "entity_id": 1,
+        "name": "EntityA",
+        "description": "More about EntityA"
+      }, {
+        "entity_id": 2,
+        "name": "EntityB",
+        "description": "More about EntityB"
+      }, {
+        "entity_id": 3,
+        "name": "EntityC",
+        "description": "More about EntityC"
+      }, {
+        "entity_id": 4,
+        "name": "EntityB",
+        "description": "More about EntityB"
+      }, {
+        "entity_id": 5,
+        "name": "EntityC",
+        "description": "More about EntityC"
+      }],
+      "links": [{
+        "from": 1,
+        "to": 2
+      }, {
+        "from": 2,
+        "to": 3
+      }, {
+        "from": 1,
+        "to": 4
+      }, {
+        "from": 4,
+        "to": 5
       }]
     };
     testEntity = new Entity(entity, entityId);
