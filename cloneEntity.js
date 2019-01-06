bbffcf37c521ceccb3577f4fda3077ca1525c7bb
Steven Lee <stevenyslee@gmail.com>
2019-01-05 16:17:51 -0800

    Fix link bug, refactor code for readability and added comments

--- cloneEntity.js
+++ cloneEntity.js
@@ -14,9 +14,10 @@ function Entity(entity, entityId) {
 }
 
 Entity.prototype.cloneEntity = function(entityId) {
-  // create adjacency list
+  // Create adjacency List
   this.createAdjacencyList();
 
+  // Create initial parents
   this.adjacencyList[entityId].from.forEach((parent) => {
     this.nodesToClone.push([entityId, parent]);
   });
@@ -25,16 +26,18 @@ Entity.prototype.cloneEntity = function(entityId) {
     let tuplet = this.nodesToClone.shift();
     let seenBefore = false;
     let newNodeId;
+
+    // Create new node if does not exist
     if (!this.ancestors[tuplet[0]]) {
       newNodeId = this.addNode(tuplet[0]);
     } else {
       seenBefore = true;
     }
 
-    // add to links for parents
+    // Add link from parent to child
     newNodeId ? this.addLink(newNodeId, tuplet[1]) : this.addLink(tuplet[0], tuplet[1]);
-    this.addLink(newNodeId || tuplet[0], tuplet[1]);
 
+    // Create traverse graph if node not seen before
     if (!seenBefore) {
       this.adjacencyList[tuplet[0]].to.forEach((child) => {
         this.nodesToClone.push([child, newNodeId]);
@@ -44,26 +47,35 @@ Entity.prototype.cloneEntity = function(entityId) {
 };
 
 Entity.prototype.addNode = function(node) {
+  let name = this.adjacencyList[node].name;
+  let entity_id = ++this.maxId;
+  let description = this.adjacencyList[node].description;
+
   let entity = { 
-    entity_id: ++this.maxId, 
-    name: this.adjacencyList[node].name
+    entity_id, 
+    name
   };
-  if (this.adjacencyList[node].description) {
-    entity.description = this.adjacencyList[node].description;
+
+  if (description) {
+    entity.description = description;
   }
+
   this.entity.entities.push(entity);
-  return this.maxId;
+
+  return entity_id;
 };
 
-Entity.prototype.addLink = function(child, parent) {
+Entity.prototype.addLink = function(to, from) {
   let link = {
-    from: parent,
-    to: child
+    from,
+    to
   };
-  this.entity.links.push(link);
+
+  return this.entity.links.push(link);
 };
 
 Entity.prototype.createAdjacencyList = function() {
+  // Create entities
   this.entity.entities.forEach((entity) => {
     if (entity.entity_id > this.maxId || this.maxId === null) {
       this.maxId = entity.entity_id;
@@ -75,7 +87,8 @@ Entity.prototype.createAdjacencyList = function() {
       from: []
     };
   });
-    console.log(this.adjacencyList);
+
+  // Create links between entities
   this.entity.links.forEach((link) => {
     this.adjacencyList[link.from].to.push(link.to);
     this.adjacencyList[link.to].from.push(link.from);
